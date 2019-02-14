7f2972eb60098db353915aaf570516288ddf09f9
Matthew Tibbals <matthewtibbals@Matthews-MBP-2.monkeybrains.net>
2019-02-14 02:10:49 -0800

    Updates

---
+++ precache-manifest.e90aeb9849e097a1b01dcfbc2c056791.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "afbe53ca9ef467e91e43",
+    "url": "/roofstock/static/css/main.ef1a41ab.chunk.css"
+  },
+  {
+    "revision": "afbe53ca9ef467e91e43",
+    "url": "/roofstock/static/js/main.afbe53ca.chunk.js"
+  },
+  {
+    "revision": "dd3f72d27b2004f56d85",
+    "url": "/roofstock/static/js/runtime~main.dd3f72d2.js"
+  },
+  {
+    "revision": "cf08f870f21e9ad49e85",
+    "url": "/roofstock/static/js/2.cf08f870.chunk.js"
+  },
+  {
+    "revision": "aa394e9c5fb04226344208fc7fbe0882",
+    "url": "/roofstock/index.html"
+  }
+];
